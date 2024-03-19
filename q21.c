fe38022ca20c603f6b43d2fb1609991ceb67fed7
Hisleno1 <99896891+Hisleno1@users.noreply.github.com>
2024-03-19 15:38:41 -0300

    Update q21.c

--- q21.c
+++ q21.c
@@ -1,1 +1,10 @@
-
+int main() {
+    int n = 10;
+    int soma = 0;
+    for (int i = 0; i < n; i++) {  // Loop externo
+        for (int j = 0; j < n; j++) {  // Loop interno
+            soma += i + j;  // Operação de soma
+        }
+    }
+    return 0;
+}
