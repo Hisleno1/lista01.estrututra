a0a4eac1bdd00ae7c1cf2b11db721bf291a1799d
Hisleno1 <99896891+Hisleno1@users.noreply.github.com>
2024-03-19 15:47:37 -0300

    Update q37.c

--- q37.c
+++ q37.c
@@ -1,1 +1,13 @@
+(a) Falso. Toda árvore binária cheia é completa, mas nem toda árvore binária completa é cheia. Por exemplo, uma árvore completa pode ter seu último nível preenchido parcialmente.
 
+(b) Falso. Nem sempre a raiz de uma árvore binária de busca Zigue-Zague é o menor elemento. Isso depende da ordem em que os elementos foram inseridos na árvore.
+
+(c) Falso. Uma árvore binária só pode ser uma coisa de cada vez: estritamente binária, cheia, completa ou Zigue-Zague. Não é possível ser todas simultaneamente.
+
+(d) Verdadeiro. Uma árvore binária pode ser estritamente binária, cheia e completa ao mesmo tempo, mas não pode ser Zigue-Zague ao mesmo tempo, pois os critérios para esses tipos de árvores são mutuamente exclusivos.
+
+(e) Falso. Se o antecessor e o sucessor de um nó são irmãos, significa que eles são filhos do mesmo nó pai, não necessariamente o próprio nó. O nó pode ser um descendente comum dos dois nós irmãos.
+
+(f) Verdadeiro. O tio de um nó é o irmão do pai desse nó, portanto, o tio é um ancestral do nó.
+
+(g) Verdadeiro. O neto de um nó é o filho de um filho desse nó, então ele é um descendente do nó.
