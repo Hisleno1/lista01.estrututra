d7c753ead42069cdd1c71e205ff30471cbb0821e
Hisleno1 <99896891+Hisleno1@users.noreply.github.com>
2024-03-19 15:14:42 -0300

    Update q7.c

--- q7.c
+++ q7.c
@@ -1,1 +1,11 @@
+def bissexto(ano):
+    if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+        return True
+    else:
+        return False
 
+ano = int(input("Digite o ano para verificar se é bissexto: "))
+if bissexto(ano):
+    print(ano, "é um ano bissexto.")
+else:
+    print(ano, "não é um ano bissexto.")
